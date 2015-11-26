f36722e19f5ab30c95a1160c7f7a07f0039035e2
EugeneFeingold <eugene.feingold@gmail.com>
2015-11-25 23:33:10 -0800

    refactor

--- main.py
+++ main.py
@@ -17,63 +17,41 @@ def main():
     return "Hello World!"
 
 
-@app.route("/color/<hex_color>")
-def handleColor(hex_color):
+def startLight(target, args):
     global process
     light.killme()
     if not process is None:
         process.terminate()
 
-    process = Process(target = light.setAll, args = [hex_color])
+    process = Process(target = target, args = args)
     process.daemon = True
     process.start()
 
-    return "ok"
+    return "Started " + target
+
+
+@app.route("/color/<hex_color>")
+def handleColor(hex_color):
+    return startLight(light.setAll, [hex_color])
 
 
 @app.route("/rainbow")
 @app.route("/rainbow/<int:msDelay>")
 def handleRainbow(msDelay = 20):
-    global process
-    light.killme()
-    if not process is None:
-        process.terminate()
-
-    process = Process(target = light.rainbowCycle, args = [msDelay])
-    process.daemon = True
-    process.start()
-
-    return "ok"
-
+    return startLight(light.rainbowCycle, [msDelay])
 
 
 @app.route("/rainbowAll")
 @app.route("/rainbowAll/<int:msDelay>")
 def handleRainbowAll(msDelay = 20):
-    global process
-    light.killme()
-    if not process is None:
-        process.terminate()
+    return startLight(light.rainbowCycleAll, [msDelay])
 
-    process = Process(target = light.rainbowCycleAll, args = [msDelay])
-    process.daemon = True
-    process.start()
-
-    return "ok"
 
 @app.route("/chaser")
 @app.route("/chaser/<int:msDelay>")
 def handleChaser(msDelay = 20):
-    global process
-    light.killme()
-    if not process is None:
-        process.terminate()
+    return startLight(light.chaser, [msDelay])
 
-    process = Process(target = light.chaser, args = [msDelay])
-    process.daemon = True
-    process.start()
-
-    return "ok"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
